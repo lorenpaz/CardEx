c28527a6b7dc390650c491116747b79e347fd108
eslozano88 <eslozano@ucm.es>
2017-06-01 13:40:42 +0200

    Add files via upload

--- src/main/java/es/ucm/fdi/iw/controller/AdminController.java
+++ src/main/java/es/ucm/fdi/iw/controller/AdminController.java
@@ -1,9 +1,11 @@
 package es.ucm.fdi.iw.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import es.ucm.fdi.iw.crawler.MagicTgIoAPI;
+import es.ucm.fdi.iw.model.Carta;
 import es.ucm.fdi.iw.model.Edicion;
 import es.ucm.fdi.iw.model.Usuario;
 
@@ -57,11 +62,50 @@ public class AdminController {
 		return "admin";
 	}
 	
+	@PostMapping("/updateCardSet")
+	@Transactional
+	public String actualizaCartasEdicion(@RequestParam("code") String code){
+		MagicTgIoAPI api = new MagicTgIoAPI();
+		Collection<Carta> cartasAPI = api.getCartasPorEdicion(code);
+		
+		for(Carta c : cartasAPI){
+			if(!existeEnBD(c.getMultiverseid()))
+				entityManager.persist(c);
+		}
+		
+		return "redirect:";
+	}
+	
 	@PostMapping("/updateSets")
 	@Transactional
 	public String actualizaEdiciones(){
-		int a = 2;
-		a = a/2;
+		MagicTgIoAPI api = new MagicTgIoAPI();
+		Collection<Edicion> edicionesAPI = api.getEdiciones();
+		Collection<Edicion> edicionesBD = entityManager.createNamedQuery("getSets").getResultList();
+ 		
+		for(Edicion edicion : edicionesAPI){
+			if(!existeEnBD(edicionesBD, edicion.getCode()))
+				entityManager.persist(edicion);
+		}
+		
 		return "redirect:";
+	}
+	
+	private boolean existeEnBD(String multiverseId){
+		Query c = entityManager.createNamedQuery("findCardByMultiverseID").setParameter("paramMultiverse", multiverseId);
+		
+		if(c.getFirstResult() == 0)
+			return false;
+		else
+			return true;
+	}
+	
+	private boolean existeEnBD(Collection<Edicion> edicionesBD, String code){
+		for(Edicion edicion : edicionesBD){
+			if(edicion.getCode().equals(code))
+				return true;
+		}
+		
+		return false;
 	}
 }
