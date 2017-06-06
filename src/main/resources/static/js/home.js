af364533cdd63ad2ca5c1cf37b46ba75bbc4456b
CharlieGnomo <carlomaroficial@gmail.com>
2017-06-06 19:56:50 +0200

    Add files via upload

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,143 +1,179 @@
-	
-function poblarListasCartas(usuarios){
-		var j;
-		for(j=0; j<usuarios.length; j++){
-			for(i = 0; i < usuarios[j].cartasPropias.length; i++) {
-				createRowCardOfrece(usuarios[j].cartasPropias[i]);
-			}
-			for(i = 0; i < usuarios[j].cartasBuscadas.length; i++) {
-				createRowCardPide(usuarios[j].cartasBuscadas[i]);
-			}
-		}
-}
-
-function poblarListaUsuarios(usuarios){
-	var j;
-	for(j=0; j<usuarios.length; j++){
-		if(j==0)
-		{
-			createRowUserFirst(usuarios[j]);
-		}else{
-		createRowUser(usuarios[j]);
-		}
-	}
-}
-
 function createRowUserFirst(user){
 	$('#intercambio-column .list-group').append(`<a href="`+user.id+`" aria-controls="tab-`+user.id+`"
-	role="tab" data-toggle="tab" class="list-group-item active">
-	`+user.usuario+`
-	<span class="badge"> <span class="glyphicon glyphicon-chevron-right"></span></span>
-	</a>
-	<c:set var="currentUser" value="`+user.usuario+`" /> `);
+			role="tab" data-toggle="tab" class="list-group-item active">
+			`+user.usuario+`
+			<span class="badge"> <span class="glyphicon glyphicon-chevron-right"></span></span>
+			</a>
+			<c:set var="currentUser" value="`+user.usuario+`" /> `);
+	addBehaviour();
 }
 
 function createRowUser(user){
 	$('#intercambio-column .list-group').append(`<a href="`+user.id+`" aria-controls="tab-`+user.id+`"
-	role="tab" data-toggle="tab" class="list-group-item usuarios">
-	`+user.usuario+`</a>`);
+			role="tab" data-toggle="tab" class="list-group-item usuarios">
+			`+user.usuario+`</a>`);
+	addBehaviour();
 }
 
 function createRowCardPide(card){
 	$('#pide-column .list-group').append(`<div class="tab-pane fade in">
-					<li class="list-group-item r">
-						<a class="nostyle" href="#" data-toggle="modal" data-target="#`
+			<li class="list-group-item r">
+			<a class="nostyle" href="#" data-toggle="modal" data-target="#`
 			+ card.id+`">`+card.name+`</a>
-						<!--Cartas -->
-						<div class="modal fade" id="`+card.id+`" role="dialog">
-							<div class="modal-dialog">
-								<!-- Modal content-->
-								<h:modalCarta cartaPropia="`+card+`" />
-							</div>
-						</div>
-					</li>
-				</div>`);
+			<!--Cartas -->
+			<div class="modal fade" id="`+card.id+`" role="dialog">
+			<div class="modal-dialog">
+			<!-- Modal content-->
+			<h:modalCarta cartaPropia="`+card+`" />
+			</div>
+			</div>
+			</li>
+	</div>`);
 }
 
 function createRowCardOfrece(card){
 	$('#ofrece-column .list-group').append(`<div class="tab-pane fade in">
-					<li class="list-group-item r">
-						<a class="nostyle" href="#" data-toggle="modal" data-target="#` + card.carta.id+`">`+card.carta.name+`</a>
-						<!--Cartas -->
-						<div class="modal fade" id="`+card.carta.id+`" role="dialog">
-							<div class="modal-dialog">
-								<!-- Modal content-->
-								<h:modalCarta cartaPropia="`+card+`" />
-							</div>
-						</div>
-					</li>
-				</div>`);
+			<li class="list-group-item r">
+			<a class="nostyle" href="#" data-toggle="modal" data-target="#` + card.carta.id+`">`+card.carta.name+`</a>
+			<!--Cartas -->
+			<div class="modal fade" id="`+card.carta.id+`" role="dialog">
+			<div class="modal-dialog">
+			<!-- Modal content-->
+			<h:modalCarta cartaPropia="`+card+`" />
+			</div>
+			</div>
+			</li>
+	</div>`);
+}
+	
+
+function poblarListasCartas(){
+	
+	$('#ofrece-column .list-group').empty();
+	for(i = 0; i < filterOwnCards.length; i++) {
+		createRowCardOfrece(filterOwnCards[i]);
+	}
+	$('#pide-column .list-group').empty();
+	for(i = 0; i < filterSearchCards.length; i++) {
+		createRowCardPide(filterSearchCards[i]);
+	}
 }
 
-function filterListas(card,user,tab) {
-	
-	if(tab == false)
-	{
-		filterByUser(user);
-		filterByCard(card,user);
-		$('.list-group').empty();
-		poblarListaUsuarios(filterUsers);
-	}else{
-		$('#ofrece-column .list-group').empty();
-		$('#pide-column .list-group').empty();
-		filterByCard(card);	
+function poblarListaUsuarios(){
+	$('#intercambio-column .list-group').empty();
+	var j;
+	for(j=0; j<filterUsers.length; j++){
+		if(j==0){
+			createRowUserFirst(filterUsers[j]);
+		}else{
+			createRowUser(filterUsers[j]);
+		}
 	}
-	poblarListasCartas(filterUsers);
-	filterUsers = usuariosJSON.usuarios;
 }
 
 function filterByUser(user){
-	filterUsers = $(filterUsers).filter(function(){
+	filterUsers = $(usuariosJSON.usuarios).filter(function(){
 		var n = this.usuario;
 		return (n.includes(user));
 	});
+	return filterUsers[0];
 }
 
-function filterByCard(card){
-	filterUsers = $(filterUsers).cartasPropias.filter(function(user){
-			return this.carta.name.includes(card);
-	});
-	filterUsers += $(filterUsers).cartasBuscadas.filter(function(user) {
-			return this.name.includes(card);
+function loadFilterUsersListInOwnCards(list){
+	for(i = 0; i < list.length; i++) {
+		var usu = filterByUser(list[i].usuarioPropietario);
+		if(filterUsersList.length>0){
+			if(isNotInside(usu)){
+				filterUsersList.push(usu);
+			}	
+		}else{
+			filterUsersList.push(usu);
+		}
+	}
+}
+
+function loadFilterUsersListInSearchCards(usu){
+	if(filterUsersList.length>0){
+		if(isNotInside(usu)){
+			filterUsersList.push(usu);
+		}	
+	}else{
+		filterUsersList.push(usu);
+	}
+}
+
+function filterCardsByUser(user){
+	var filterUser = filterByUser(user);
+	if(filterUser != undefined){
+		filterOwnCards = filterUser.cartasPropias;
+		filterSearchCards = filterUser.cartasBuscadas;
+		filterUsersList = filterUsers;
+		currentUser = filterUsersList[0];
+	}else{
+		filterUsersList = [];
+	}
+}
+
+function filterCardsByName(card){
+	var filterAux = filterUsers;
+	var i;
+	for(i=0; i<filterUsers.length; i++){
+		filterOwnCards = filterUsers[i].cartasPropias.filter(function(c){
+			return c.carta.name.includes(card);
 		});
-} 
+		if(filterOwnCards>0){
+			loadFilterUsersListInOwnCards(filterOwnCards);
+			filterAux[i].cartasPropias = filterUsers[i].cartasPropias;
+		}
+		filterSearchCards = filterUsers[i].cartasBuscadas.filter(function(c){
+			return c.name.includes(card);
+		});
+		if(filterSearchCards>0){
+			loadFilterUsersListInSearchCards(filterUser);
+			filterAux[i].cartasBuscadas = filterUsers[i].cartasBuscadas;
+		}
+	}
+	filterUsers = filterAux;
+	currentUser = filterUsersList[0];
+}
+
+function isNotInside(user){
+	var res = $(filterUsersList).filter(function(){
+		var n = this.usuario;
+		return (n.includes(user.usuario));
+	});
+	return res.length == 0;
+}
 
 
-var filterUsers;
-var tab = false;
+function filterCardsByCardAndUser(user, card){
+	if((card == undefined || card == "")&&(user== undefined || user == "")){	
+		filterUsers = usuariosJSON.usuarios;
+		filterUsersList = filterUsers;
+		currentUser = filterUsers[0];
+		filterOwnCards = currentUser.cartasPropias;
+		filterSearchCards = currentUser.cartasBuscadas;
+	}else{
+		if(user!=""){
+			filterCardsByUser(user);
+		}
+		if(card != ""){
+			filterCardsByName(card);
+		}
+	}
+}
 
-$( function(){
-	
-	filterUsers = usuariosJSON.usuarios;
-
-    $("#cartaFilter").on('keyup', function(){	
-	    var card = $(this).val();
-        var user = $('#usuarioFilter').val();
-        
-		filterListas(card,user);
-    });
-    
-    $("#usuarioFilter").on('keyup', function(){
-    	
-    	var user = $('#usuariosFilter').val();
-        var card = $('#cartaFilter').val();
-        
-        filterListas(card,user);
-     });
-    
-    
-	//Cambiar botón al pulsar en el usuario
-	$("#intercambio-column .list-group-item").click(function(){
+function addBehaviour(){
+	// Cambiar botón al pulsar en el usuario
+	$("#intercambio-column a").click(function(){
 	    if (!$(this).hasClass('active')){
 	    		
-	    	var usuario = $(this).text().replace(/\s/g,'');
-	    	
-	        //Filtro
-	        var user = usuario;
-	        tab = true;
-	        var card = $('#cartaFilter').val();
-	        filterListas(card,user,tab);
-	        
+	    	var user = $(this).text().replace(/\s/g,'');
+	    	var card = $('#cartaFilter').val();
+	        filterUsu = filterByUser(user);
+	        currentUser = filterUsu;
+	        filterCardsByCardAndUser(user, card);
+	        poblarListasCartas();
 	    	var anterior = $('.active').addClass('usuarios').removeClass('active');
 	    	$('.badge').remove();
 	    	$('#usuarioQuieroIntercambio').remove();
@@ -149,16 +185,84 @@ $( function(){
 	        cartasDelUsuario = "."+cartasDelUsuario;
 	        
 	        
-	        document.getElementById('intercambioEnviar').action = "../intercambio/"+usuario;
+	        document.getElementById('intercambioEnviar').action = "../intercambio/"+user;
 	        $(this).append('<span class="badge"> <span class="glyphicon glyphicon-chevron-right"></span></span>');
-	        
+	        filterUsers = usuariosJSON.usuarios;
+	        filterUsersList = filterUsers;
 	    }
 	});
+}
+
+
+var filterUsers;
+var filterUsersList;
+var filterOwnCards;
+var filterSearchCards;
+var ownCardsFilterBy;
+var serachCardsFilterBy;
+var tab = false;
+var currentUser;
+
+$( function(){
+	
+	filterUsers = usuariosJSON.usuarios;
+	filterUsersList = [];
+	if(filterUsers.length>0){
+		filterOwnCards = filterUsers[0].cartasPropias;
+		filterSearchCards = filterUsers[0].cartasBuscadas;
+		currentUser = filterUsers[0];
+	}
+
+    $("#cartaFilter").on('keyup', function(){	
+	    var card = $(this).val();
+        var user = $('#usuarioFilter').val();
+        filterCardsByCardAndUser(user, card);
+        poblarListasCartas();
+        poblarListaUsuarios();
+    });
+    
+    $("#usuarioFilter").on('keyup', function(){
+    	
+    	var user = $(this).val();
+        var card = $('#cartaFilter').val();
+        filterCardsByCardAndUser(user, card);
+        poblarListasCartas();
+        poblarListaUsuarios();
+     });
+    
+    addBehaviour();/*
+	// Cambiar botón al pulsar en el usuario
+	$("#intercambio-column a").click(function(){
+	    if (!$(this).hasClass('active')){
+	    		
+	    	var user = $(this).text().replace(/\s/g,'');
+	    	var card = $('#cartaFilter').val();
+	        filterUsu = filterByUser(user);
+	        currentUser = filterUsu;
+	        filterCardsByCardAndUser(user, card);
+	        poblarListasCartas();
+	    	var anterior = $('.active').addClass('usuarios').removeClass('active');
+	    	$('.badge').remove();
+	    	$('#usuarioQuieroIntercambio').remove();
+	    	
+	        $(this).addClass('active');
+	        $(this).removeClass('usuarios');
+	        
+	        var cartasDelUsuario = $(this).attr('href');
+	        cartasDelUsuario = "."+cartasDelUsuario;
+	        
+	        
+	        document.getElementById('intercambioEnviar').action = "../intercambio/"+user;
+	        $(this).append('<span class="badge"> <span class="glyphicon glyphicon-chevron-right"></span></span>');
+	        filterUsers = usuariosJSON.usuarios;
+	        filterUsersList = filterUsers;
+	    }
+	});*/
 	
 });
 
 
-//Enviar oferta
+// Enviar oferta
 function enviarOferta(e) {
 
 	var usuarioIntercambio = e.target;
